51ac6ad0ada6582bdd5bc2131b42dbe1680e2683
Ahmad Fwity <ahmad.fwaity@gmail.com>
2025-02-01 19:44:19 +0300

    ahmad

--- src/components/EmailInput/EmailInput.jsx
+++ src/components/EmailInput/EmailInput.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import './EmailInput.css'
 import ButtonBh from '../ButtonBh/ButtonBh'
 
--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,5 +1,5 @@
 
-import ArtistPageTaps from '../../components/ArtistPageTaps/ArtistPageTaps'
+import RankingCardsSection from '../../components/RankingCardsSection/RankingCardsSection'
 import RankingHero from '../../components/RankingHero/RankingHero'
 import './Ranking.css'
 
@@ -11,8 +11,7 @@ export default function Ranking() {
     <RankingHero/>
     </div>
     <div className="ranking-body">
-    <ArtistPageTaps tab1={'hn-tab1Ranking'} tab2={'hn-tab2Ranking'} tab3={'hn-tab3Ranking'} tab4={'hn-tab4Ranking'}  number1={'hn-302Ranking'} number2={'hn-67Ranking'} number3={'hn-4Ranking'} title1={'Today'} title2={'This Week'} title3={'This Month'} title4={'All Time'}/>
-    
+    <RankingCardsSection/>
     </div>
     </>
   )
